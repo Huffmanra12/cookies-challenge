952ef10514fc0972bb49cf009de4edb47e65ebbb
Richard <huffmanra12@gmail.com>
2023-06-27 15:19:39 -0600

    removed stray code

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
 const cookieSession = require('cookie-session')
 const app = express();
 const port = 8080;
+
 app.use(express.json());
-app.use(cookieParser());
+
 app.use(cookieSession({
   name: 'user',
   httpOnly:true,
